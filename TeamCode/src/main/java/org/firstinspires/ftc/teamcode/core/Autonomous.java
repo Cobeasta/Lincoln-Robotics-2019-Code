00ad5fa9873b9d4299286ca30a06e24f5c1dd8a0
Cobeasta <stopyloknok@gmail.com>
2018-09-13 19:52:44 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Autonomous.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.core;
+
+public class Autonomous {
+
+
+    public void loop(){
+
+    }
+
+    /**
+     * This method will lower the robot from the platform
+     * and return true once it is done.
+     */
+    protected boolean land(){
+    return true;
+}
+
+    /**
+     *
+     */
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Robot.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.core;
+
+public class Robot {
+//Store hardware as static variables. at the top.
+
+
+
+}
