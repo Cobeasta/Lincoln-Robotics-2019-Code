c108eb71fe7677d598b9b11ba5fea55a48e4ac08
Cobeasta <stopyloknok@gmail.com>
2018-09-21 22:17:00 -0500

    Added documentation to constants class and programmed the ability to set a min and max to the output of the pid loops.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.util;
 
+/**
+ * Here, store the names for each of the motors in the hardware map,
+ * store the pid constants for each of the loops,
+ * and store any other constants that are used in the code.
+ */
 public class Constants {
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PID.java
@@ -10,30 +10,55 @@ public class PID
     private long timeStamp, prevTimeStamp;
     private double kP, kI, kD, kF;
     private double error, previousError;
+    private double deadband = .05;
+    private double min=-1, max = 1;
+    private double setpoint = 0;
 public PID(double kP, double kI, double kD, double kF)
 {
     this.kP = kP;
     this.kI = kI;
     this.kD = kD;
     this.kF = kF;
+    previousError = 0;
 }
 
-
-    public double update()
+    /**
+     * Calculates the next output for the pid, then returns the output.
+     * @return
+     */
+    public double update(double input)
     {
+        error = setpoint - input;
      double output = calculateP() -calculateD();
       error = previousError;
-      return output;
+      return Util.trim(output, min, max);
     }
+
+    /**
+     *
+     * @return the error multiplied by the kP term.
+     */
     public double calculateP()
     {
         return kP * error;
     }
+
+    /**
+     * The rate of change for the error with respect to time.
+     * @return
+     */
     public double calculateD()
     {
         return kD *((error - previousError)/(double)(timeStamp - prevTimeStamp));
     }
 
+
+    public double getSetpoint(){
+        return setpoint;
+    }
+
+
+//Getters and setters for variables.
     public double getP(){
     return kP;
     }
@@ -47,6 +72,12 @@ public PID(double kP, double kI, double kD, double kF)
     public double getF(){
         return kF;
     }
+    public double getMax(){
+        return max;
+    }
+    public double getMin(){
+        return  min;
+    }
 
     public void setP(double p){
     kP = p;
@@ -59,6 +90,15 @@ public PID(double kP, double kI, double kD, double kF)
     }
     public void setF(double f){
     kF = f;
+    }
+    public void setMax(double max){
+        this.max = max;
+    }
+    public void setMin(double min){
+        this.min = min;
+    }
+    public void setSetpoint(double target){
+        setpoint = target;
     }
 
 }
